8ad5e7a49c54484fc99c30c837383ac62601c5b5
unknown <amarkarer@gmail.com>
2020-02-07 17:30:33 +0530

    Changes forking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { AddUserComponent } from './add-user/add-user.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import {UserService} from '../user.service'
+import {UserService} from '../../user.service'
 
 @Component({
   selector: 'app-add-user',
